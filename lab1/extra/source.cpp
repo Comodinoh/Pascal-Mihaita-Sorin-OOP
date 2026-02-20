e463ba0d3fdea86d27c8ca4fe8a43bc2d87bc2a8
Mihaita Sorin Pascal <comodinoh@pm.me>
2026-02-20 22:16:20 +0000

    Solved: Lab1 extra

---
+++ lab1/extra/source.cpp
@@ -0,0 +1,59 @@
+#include "header.h"
+
+int Sum(int a, int b) { return a + b; }
+int Dif(int a, int b) { return a - b; }
+int Mul(int a, int b) { return a * b; }
+int Div(int a, int b) { return a / b; }
+
+int main(int argc, char* argv[])
+{
+    char input[] = "---H***E+++L+++L///O---P+++O/+-**O---";
+    func Operatori[] = {Sum, Dif, Mul, Div};
+    int S, V;
+    Content x;
+    int idx;
+
+    for (int i = 0; i < strlen(input); i++)
+    {
+        switch (input[i] - 42)
+        {
+            case INMULTIRE:
+                {
+                    idx = 2;
+                    x.p1 = 3;
+                    x.p2 = 3;
+                    break;
+                }
+            case SUMA:
+                {
+                    idx = 0;
+                    x.p1 = 7;
+                    x.p2 = 5;
+                    break;
+                }
+            case DIFERENTA:
+                {
+                    idx = 1;
+                    x.p1 = 10;
+                    x.p2 = 1;
+                    break;
+                }
+            case IMPARTIRE:
+                {
+                    idx = 3;
+                    x.p1 = 8;
+                    x.p2 = 4;
+                    break;
+                }
+        }
+
+        V = Operatori[idx](x.p1, x.p2);
+
+        S += V;
+    }
+    
+    //S=337
+    printf("S = %d\n", S);
+
+    return 0;
+}
